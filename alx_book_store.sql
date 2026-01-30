86c5c08def8e26f5c384b1a058d57232262a1656
Amr Wahidi <amrw5189@gmail.com>
2026-01-30 15:59:30 +0200

    fix sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,9 +1,9 @@
-CREATE TABLE AUTHORS (
+CREATE TABLE IF NOT EXISTS AUTHORS (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE BOOKS(
+CREATE TABLE IF NOT EXISTS BOOKS(
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -15,21 +15,21 @@ CREATE TABLE BOOKS(
 
 
 
-CREATE TABLE CUSTOMERS (
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE ORDERS (
+CREATE TABLE IF NOT EXISTS ORDERS (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
 );
 
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
